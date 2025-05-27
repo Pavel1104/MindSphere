039d52852c18e720d7f877b15bfe2a290ff3a36f
Pavlo I <ipa0486@gmail.com>
2025-05-27 22:17:10 +0300

    Added test pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  i18n: {
+    locales: ["ua", "ru", "pl", "en"], // Доступні локалі
+    defaultLocale: "ua",
+  },
 };
 
 export default nextConfig;
---
+++ src/app/pl/page.tsx
@@ -0,0 +1,8 @@
+export default function PlPage() {
+  return (
+    <div>
+      <h1>Witamy w MindSphere!</h1>
+      <p>Platforma do rozwoju i samodoskonalenia.</p>
+    </div>
+  );
+}
